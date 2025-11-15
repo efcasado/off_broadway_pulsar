c464fb88c8a39418d5f0f7eb0563502b8d1eacf7
Enrique Fernandez <enrique@bluelabs.eu>
2025-11-15 16:03:51 +0100

    fix: do not start pulsar app automatically

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule OffBroadwayPulsar.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      included_applications: [:pulsar]
     ]
   end
 
