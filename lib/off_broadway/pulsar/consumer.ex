b106e7c7ed3ad7249f6e8c72ab8669f566a841a8
Enrique Fernández <enrique@bluelabs.eu>
2025-12-03 18:34:56 +0100

    fix: wrong function clause in consumer

--- lib/off_broadway/pulsar/consumer.ex
+++ lib/off_broadway/pulsar/consumer.ex
@@ -11,17 +11,9 @@ defmodule OffBroadway.Pulsar.Consumer do
   end
 
   @impl true
-  def handle_message({command, metadata, {msg_metadata, msg_payload}, broker_metadata, message_id_to_ack}, state) do
-    # message_id_to_ack has the correct batch_index set for batch messages
-    message_info = %{
-      message_id: message_id_to_ack,
-      command: command,
-      metadata: metadata,
-      payload: {msg_metadata, msg_payload},
-      broker_metadata: broker_metadata
-    }
-
-    send(state.broadway_producer, {:pulsar_message, message_info})
+  def handle_message(%Pulsar.Message{} = message, state) do
+    # Send the Pulsar.Message struct directly to the Broadway producer
+    send(state.broadway_producer, {:pulsar_message, message})
 
     # Return {:noreply, state} to use manual ACK mode
     # Broadway will handle ACK/NACK through the acknowledger
